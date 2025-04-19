6a14fa397357261960d2cdd8f540d5f8a59e475e
Diogo Oliveira <diogo.o@icloud.com>
2025-04-19 14:53:30 -0300

    feat: add DashboardView and enhance LoginView with form handling and authentication logic

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Dashboard</h1>
+</template>
+
+<style></style>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,9 +1,38 @@
 <template>
-  <div class="about">
-    <h1>Login</h1>
+  <div class="login-container">
+    <form class="login-form" @submit.prevent="handleLogin">
+      <input v-model="email" type="email" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Senha" />
+      <input type="hidden" :value="deviceName" name="device_name" />
+      <button type="submit">Entrar</button>
+    </form>
   </div>
 </template>
 
-<style>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 
-</style>
+const email = ref<string>('')
+const password = ref<string>('')
+const deviceName = ref<string>('')
+
+const auth = useAuthStore()
+const router = useRouter()
+
+onMounted(() => {
+  const userAgent = navigator.userAgent
+  const platform = navigator.platform
+  deviceName.value = `${platform} - ${userAgent}`
+})
+
+const handleLogin = async () => {
+  try {
+    await auth.login(email.value, password.value, deviceName.value)
+    router.push('/dashboard')
+  } catch (error) {
+    alert('Erro ao fazer login.')
+  }
+}
+</script>

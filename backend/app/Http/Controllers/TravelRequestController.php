e2447f2b98ad80ce2c93c5bbe551e0bad1631856
Diogo Oliveira <diogo.o@icloud.com>
2025-04-21 14:52:17 -0300

    feat: implement TravelRequestController for managing travel requests

---
+++ backend/app/Http/Controllers/TravelRequestController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TravelRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use App\Repositories\TravelRequestRepository;
+use App\Http\Requests\ChangeStatusTravelRequest;
+use App\Http\Requests\StoreTravelRequestRequest;
+use App\Http\Requests\UpdateTravelRequestRequest;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class TravelRequestController extends Controller
+{
+    /**
+     * @param TravelRequestRepository $travelRequestRepository
+     */
+    public function __construct(
+        protected TravelRequestRepository $travelRequestRepository
+    )
+    {
+    }
+
+    /**
+     * @return Response
+     */
+    public function index(): Response
+    {
+        try {
+
+            return response([
+                'travelRequests' => $this->travelRequestRepository->listByUser(),
+            ], ResponseAlias::HTTP_OK);
+        } catch (\Throwable $e) {
+            Log::error('Erro ao listar pedidos: ' . $e->getMessage());
+
+            return response([
+                'error' => 'Erro ao listar pedidos.',
+            ], ResponseAlias::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+   
+}

fff2c73dc76aaa8193baa900de65b5a305d8f440
Diogo Oliveira <diogo.o@icloud.com>
2025-04-22 08:59:19 -0300

    feat: add Vuetify and Material Design Icons support to the frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,6 +9,26 @@ import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'     
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
 const app = createApp(App)
 
 const pinia = createPinia()
@@ -16,6 +36,7 @@ pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
 
